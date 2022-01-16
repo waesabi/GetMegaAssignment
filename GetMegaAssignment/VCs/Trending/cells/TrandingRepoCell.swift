bfb2e7d3cbb19e79d4a88b76b2c9a6af4e41c9ca
Sanket Kumar <sanket.kumar@indwealth.in>
2022-01-16 14:55:16 +0530

    multiline message, hide if value is nil

--- GetMegaAssignment/VCs/Trending/cells/TrandingRepoCell.swift
+++ GetMegaAssignment/VCs/Trending/cells/TrandingRepoCell.swift
@@ -21,8 +21,14 @@ class TrandingRepoCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bottomInfoStackView: UIStackView!
     @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var languageStackView: UIStackView!
     @IBOutlet weak var languageLabel: UILabel!
+    
+    @IBOutlet weak var starStackView: UIStackView!
     @IBOutlet weak var starLabel: UILabel!
+    
+    @IBOutlet weak var forkStackView: UIStackView!
     @IBOutlet weak var forkLabel: UILabel!
     
     override func awakeFromNib() {
@@ -40,13 +46,17 @@ class TrandingRepoCell: UITableViewCell {
         self.loginNameLabel.text = repo.owner?.login
         self.nameLabel.text = repo.fullName
         self.descriptionLabel.text = repo.description
+        
         self.languageLabel.text = repo.language
+        self.languageStackView.isHiddenInStackView = repo.language == nil
         
         let starCount = repo.watchers ?? 0
         self.starLabel.text = "\(starCount)"
+        self.starStackView.isHiddenInStackView = repo.watchers == nil
         
         let forkCount = repo.forksCount ?? 0
         self.forkLabel.text = "\(forkCount)"
+        self.forkStackView.isHiddenInStackView = repo.forksCount == nil
         
         let isExpanded = repo.isExpanded ?? false
         self.bottomInfoStackView.isHiddenInStackView = !isExpanded
